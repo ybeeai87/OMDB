ecd5d41975c4fbf3d3989f0ad2061d0fb867d648
Yousif <ybeeai@gmail.com>
2021-11-03 16:49:43 -0400

    commit

---
+++ SearchingOMDB/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SearchingOMDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchingOMDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieDAL movieDAL = new MovieDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieSearch(string title)
+        {
+            MovieModel result = movieDAL.GetMovie(title);
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult MovieNight(string title, string title2, string title3)
+        {
+            MovieModel result1 = movieDAL.GetMovie(title);
+            MovieModel result2 = movieDAL.GetMovie(title2);
+            MovieModel result3 = movieDAL.GetMovie(title3);
+            List<MovieModel> movielist = new List<MovieModel>();
+            movielist.Add(result1);
+            movielist.Add(result2);
+            movielist.Add(result3);
+            return View(movielist);
+        }
+
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SearchingOMDB/Models/MovieDAL.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SearchingOMDB.Models
+{
+    public class MovieDAL
+    {
+        public MovieModel GetMovie(string title)
+        {
+            string url = $"http://www.omdbapi.com/?apikey=207f897b&t={title}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+
+            return result;
+        }
+    }
+}
